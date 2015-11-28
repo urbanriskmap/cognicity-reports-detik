3b0b17c2b9471363329cc1be4c0ec591940e5f75
Tomas <tomas@uow.edu.au>
2015-11-28 15:18:01 +0700

    Fixed #5

--- schema/update_schema_detik.sql
+++ schema/update_schema_detik.sql
@@ -4,7 +4,7 @@ CREATE OR REPLACE FUNCTION public.update_all_reports_from_detik()
 $BODY$
 	BEGIN
 		IF (TG_OP = 'UPDATE') THEN
-			INSERT INTO all_reports (fkey, created_at, text, source, lang, url, image_url, title, the_geom) SELECT NEW.pkey, NEW.created_at, NEW.text, 'detik', NEW.url, NEW.image_url, NEW.title, NEW.the_geom;
+			INSERT INTO all_reports (fkey, created_at, text, source, lang, url, image_url, title, the_geom) SELECT NEW.pkey, NEW.created_at, NEW.text, 'detik', NEW.lang, NEW.url, NEW.image_url, NEW.title, NEW.the_geom;
 			RETURN NEW;
 		ELSIF (TG_OP = 'INSERT') THEN
 			INSERT INTO all_reports (fkey, created_at, text, source, lang, url, image_url, title, the_geom) SELECT NEW.pkey, NEW.created_at, NEW.text, 'detik', NEW.lang, NEW.url, NEW.image_url, NEW.title, NEW.the_geom;
